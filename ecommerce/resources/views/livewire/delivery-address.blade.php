5e967492afc2d575fcb8405865807b7cd116aea3
Kito <surya.aw26@gmail.com>
2023-02-15 16:15:26 +0700

    - Add button "tambah alamat" when customer is not have added address in checkout - Change the number show to dynamically not static in search result

--- ecommerce/resources/views/livewire/delivery-address.blade.php
+++ ecommerce/resources/views/livewire/delivery-address.blade.php
@@ -1,10 +1,13 @@
 <div>
     <div class="cf-title">Alamat Pengiriman</div>
     <div class="row m-0 p-0">
-        @foreach ($userInfo as $row)
-        <p class="m-0 p-0 fw-bold">{{ "$row->first_name $row->last_name" }}</p>
-        <p class="m-0 p-0">{{ $row->phone_number }}</p>
-        <p class="p-0">{{ $row->address }}</p>
-        @endforeach
+        <p class="m-0 p-0 fw-bold">{{ "$userInfo->first_name $userInfo->last_name" }}</p>
+        <p class="m-0 p-0">{{ $userInfo->phone_number }}</p>
+        @if ($userInfo->address !== null)
+        <p class="p-0">{{ $userInfo->address }}</p>
+        @endif
     </div>
+    @if ($userInfo->address === null)
+    <a class="btn btn-outline-dark btn-sm mt-3 mb-3" role="button">Tambah alamat</a>
+    @endif
 </div>
