2fc7f463fecfaffafb58709229f6d443ae86f727
Kito <surya.aw26@gmail.com>
2023-02-18 20:40:09 +0700

    Add subquery in wishlist to get list size

--- ecommerce/app/Http/Livewire/Wishlist.php
+++ ecommerce/app/Http/Livewire/Wishlist.php
@@ -12,12 +12,15 @@ class Wishlist extends Component
 
     public function render()
     {
+        $sizeSubquery = DB::table('detail_products')
+            ->selectRaw("dp_id, GROUP_CONCAT(size SEPARATOR ', ') AS size, GROUP_CONCAT(stock SEPARATOR ', ') AS stock")
+            ->groupBy('dp_id');
+
         $this->wishlists = DB::table('wishlists')
             ->join('products', 'wishlists.product_id', 'products.id')
-            // ->leftJoin('detail_products', function ($join) {
-            //     $join->on('wishlists.product_id', 'detail_products.dp_id')
-            //         ->on('detail_products.size', 'carts.size');
-            // })
+            ->joinSub($sizeSubquery, 'sizeSubquery', function ($join) {
+                $join->on('sizeSubquery.dp_id', 'wishlists.product_id');
+            })
             ->where('wishlists.user_id', Auth::id())
             ->select(
                 'wishlists.id AS WishlistID',
@@ -25,12 +28,26 @@ class Wishlist extends Component
                 'products.product_id',
                 'products.name AS ProdName',
                 'products.price',
+                'sizeSubquery.size',
+                'sizeSubquery.stock',
                 DB::raw("DATE_FORMAT(wishlists.created_at, '%Y-%m-%d') AS created_at"),
-                // DB::raw("IFNULL(detail_products.stock, 0) AS AvailStock"),
                 'products.image'
             )
-            ->get();
+            ->get()->toArray();
+
+        foreach ($this->wishlists as $row) {
+            $newSize = explode(', ', $row->size);
+            $row->size = $newSize;
+
+            $newStock = explode(', ', $row->stock);
+            $row->stock = $newStock;
+        }
 
         return view('livewire.wishlist');
+    }
+
+    public function addToCart($wishlistID, $ProductID)
+    {
+        dd("$wishlistID dan $ProductID");
     }
 }
--- ecommerce/resources/views/livewire/wishlist.blade.php
+++ ecommerce/resources/views/livewire/wishlist.blade.php
@@ -1,4 +1,15 @@
 <div class="cart-table-warp">
+    {{-- Modal --}}
+    <div class="modal" id="wishlistModal" tabindex="-1" role="dialog" wire:ignore>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- End of Modal --}}
+
     <table id="cartform">
         @foreach ( $wishlists as $row )
         <tr>
@@ -13,15 +24,22 @@
                 </div>
             </td>
             <td>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected disabled>Pilih size</option>
+                    @foreach ($row->size as $item)
+                    <option value="{{ $item }}">{{ $item }}</option>
+                    @endforeach
+                </select>
+            </td>
+            <td>
                 <div class="float-end me-4">
-                    <a onclick="confirm('Yakin?') || event.stopImmediatePropagation()"
-                        wire:click.prevent="remove('{{ $row->WishlistID }}', '{{ $row->ProductID }}')"
+                    <a wire:click.prevent="addToCart('{{ $row->WishlistID }}', '{{ $row->ProductID }}')"
                         class="btn btn-sm btn-info d-block">
                         Tambahkan ke keranjang
                     </a>
-                    <a onclick="confirm('Yakin?') || event.stopImmediatePropagation()"
-                        wire:click.prevent="remove('{{ $row->WishlistID }}', '{{ $row->ProductID }}')"
-                        class="btn btn-sm btn-danger d-block mt-2">
+                    <a wire:click.prevent="remove('{{ $row->WishlistID }}', '{{ $row->ProductID }}')"
+                        class="btn btn-sm btn-danger d-block mt-2" data-bs-toggle="modal"
+                        data-bs-target="#wishlistModal">
                         Hapus
                     </a>
                 </div>
