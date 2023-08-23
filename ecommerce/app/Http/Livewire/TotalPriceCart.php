cfacf179ebf990bd9ffa594eb68a16e05b39d46e
Kito <surya.aw26@gmail.com>
2023-08-23 11:55:42 +0700

    feat(cart): update qty cart without login

--- ecommerce/app/Http/Livewire/TotalPriceCart.php
+++ ecommerce/app/Http/Livewire/TotalPriceCart.php
@@ -14,11 +14,37 @@ class TotalPriceCart extends Component
 
     public function render()
     {
-        $total = DB::table('carts')
-            ->join('products', 'carts.product_id', '=', 'products.id')
-            ->where('carts.user_id', Auth::id())
-            ->selectRaw('SUM(products.price * carts.qty) AS price')
-            ->value('price');
+        $total = 0;
+
+        if (Auth::check()) {
+            $total = DB::table('carts')
+                ->join('products', 'carts.product_id', '=', 'products.id')
+                ->where('carts.user_id', Auth::id())
+                ->selectRaw('SUM(products.price * carts.qty) AS price')
+                ->value('price');
+        }
+
+        if (!Auth::check() && isset($_COOKIE['cart_id']) && isset($_COOKIE['carts'])) {
+            $dataArray = json_decode($_COOKIE['carts'], true);
+
+            $results = DB::table('products')
+                ->join('detail_products', 'products.id', '=', 'detail_products.dp_id')
+                ->select('products.id AS ProductID', 'products.price AS price')
+                ->whereIn('detail_products.dp_id', array_column($dataArray, 'product_id'))
+                ->get();
+
+            // Processing and formatting the results...
+            foreach ($results as &$result) {
+                $productId = $result->ProductID;
+
+                foreach ($dataArray as $item) {
+                    if ($item['product_id'] == $productId) {
+                        $total += $result->price * $item['quantity'];
+                        break;
+                    }
+                }
+            }
+        }
 
         $this->total = rupiah($total);
 
