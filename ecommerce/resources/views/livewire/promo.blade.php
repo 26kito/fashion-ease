3d1043fd1b2e501d48748f65f984434c451cadf0
Kito <surya.aw26@gmail.com>
2023-11-01 14:24:13 +0700

    feat(voucher)

--- ecommerce/resources/views/livewire/promo.blade.php
+++ ecommerce/resources/views/livewire/promo.blade.php
@@ -5,7 +5,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Pakai Promo</h5>
-                    {{-- <a href="#" class="btn">Reset Promo</a> --}}
                     <a href="#" class="btn btn-reset disabled">Reset Promo</a>
                 </div>
                 <div class="modal-body">
@@ -14,7 +13,7 @@
                             <input type="search" class="form-control" name="" id="" placeholder="Masukkan kode promo">
                         </div>
                         <div class="col-md-4">
-                            <a href="#" class="btn">Terapkan</a>
+                            <a href="#" class="btn disabled">Terapkan</a>
                         </div>
                     </div>
                     <br>
@@ -24,13 +23,26 @@
                         <div class="col-md-12">
                             @foreach ($vouchers as $row)
                             <div class="card mb-3">
-                                <div class="card-body voucher-card" data-voucher-id="{{ $row->id }}" style="cursor: pointer" id="voucherCard">
-                                {{-- <div wire:click='setVoucher' class="card-body voucher-card" data-voucher-id="{{ $row->id }}" style="cursor: pointer" id="voucherCard"> --}}
+                                <div class="card-body voucher-card"
+                                    data-voucher-id="{{ $row->id }}" style="cursor: pointer" id="voucherCard">
                                     <p class="fw-bold">{{ $row->title }}</p>
                                 </div>
                             </div>
                             @endforeach
                         </div>
+                    </div>
+                </div>
+                <div class="row mb-4 voucher-calculate d-none">
+                    <div class="col-lg-8 col-md-8 col-sm-8">
+                        <div class="row">
+                            <p class="ms-3">
+                                Kamu bisa hemat<br>
+                                <b><span style="font-size: 25px"></span></b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 ms-auto">
+                        <a href="#" class="btn btn-success py-3">Pakai Promo</a>
                     </div>
                 </div>
             </div>
@@ -43,25 +55,52 @@
 </div>
 
 @push('script')
-    <script>
-        $('.voucher-card').on('click', function() {
-            let voucherID = $(this).data('voucher-id');
+<script>
+    let selectedVouchers = [];
+    $('.voucher-card').on('click', function() {
+        let voucherID = $(this).data('voucher-id');
+        let index = selectedVouchers.indexOf(voucherID);
 
-            if ($(this).hasClass('selected')) {
-                $(this).css({
-                    'background-color': 'white'
-                });
+        if ($(this).hasClass('selected')) {
+            $(this).css({
+                'background-color': 'white'
+            });
 
-                $('.btn-reset').addClass('disabled');
-                $(this).removeClass('selected');
-            } else {
-                $(this).css({
-                    'background-color': '#00AA5B'
-                });
+            $('.btn-reset').addClass('disabled');
+            $(this).removeClass('selected');
 
-                $('.btn-reset').removeClass('disabled');
-                $(this).addClass('selected');
+            // If voucherID is in the array, remove it using splice
+            if (index !== -1) {
+                selectedVouchers.splice(index, 1);
             }
+        } else {
+            $(this).css({
+                'background-color': '#00AA5B'
+            });
+
+            $('.btn-reset').removeClass('disabled');
+            $(this).addClass('selected');
+
+            if (!selectedVouchers.includes(voucherID)) {
+                selectedVouchers.push(voucherID);
+            }
+        }
+
+        if (selectedVouchers.length > 0) {
+            $('.voucher-calculate').removeClass('d-none')
+        } else {
+            $('.voucher-calculate').addClass('d-none')
+        }
+    });
+
+
+    $('.btn-reset').on('click', () => {
+        $('.voucher-card').removeClass('selected');
+        $('.btn-reset').addClass('disabled');
+
+        $('.voucher-card').css({
+            'background-color': 'white'
         });
-    </script>
+    })
+</script>
 @endpush
