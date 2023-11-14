5ba12eb0d9b81776b8402af5d2794db784a108db
Kito <surya.aw26@gmail.com>
2023-11-14 15:23:46 +0700

    style: remove unused code

--- ecommerce/app/Http/Livewire/TotalPriceCart.php
+++ ecommerce/app/Http/Livewire/TotalPriceCart.php
@@ -33,28 +33,6 @@ class TotalPriceCart extends Component
             $total = $query->value('price');
         }
 
-        // kode lama
-        // if (!Auth::check() && isset($_COOKIE['cart_id']) && isset($_COOKIE['carts'])) {
-        //     $dataArray = json_decode($_COOKIE['carts'], true);
-
-        //     $results = DB::table('products')
-        //         ->join('detail_products', 'products.id', '=', 'detail_products.dp_id')
-        //         ->select('products.id AS ProductID', 'products.price AS price')
-        //         ->whereIn('detail_products.dp_id', array_column($dataArray, 'product_id'))
-        //         ->get();
-
-        //     // Processing and formatting the results...
-        //     foreach ($results as &$result) {
-        //         $productId = $result->ProductID;
-
-        //         foreach ($dataArray as $item) {
-        //             if ($item['product_id'] == $productId) {
-        //                 $total += $result->price * $item['quantity'];
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
         if (!Auth::check() && isset($_COOKIE['cart_id']) && isset($_COOKIE['carts'])) {
             $dataArray = json_decode($_COOKIE['carts'], true);
 
