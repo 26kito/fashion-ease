f3f51e91504f887a03eead24b2d88b40c74c99a4
kito <surya.aw26@gmail.com>
2024-07-15 08:23:25 +0700

    change state menu in header and change search result title

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,12 +4,16 @@
         <div class="container">
             <!-- menu -->
             <ul class="main-menu">
-                <li><a href="{{ route('home') }}">Home</a></li>
+                <li>
+                    <a href="{{ (request()->route()->getName() == 'home') ? '#' : route('home') }}" style="color: {{ (request()->route()->getName() == 'home') ? '#f51167' : ''}}">Home</a>
+                </li>
                 <li><a href="#">Women</a></li>
                 <li><a href="#">Jewelry
                     <span class="new">New</span>
                 </a></li>
-                <li><a href="{{ route('category') }}">Category</a></li>
+                <li>
+                    <a href="{{ (request()->route()->getName() == 'category' ? '#' : route('category') ) }}" style="color: {{ (request()->route()->getName() == 'category') ? '#f51167' : ''}}">Category</a>
+                </li>
                 <li><a href="#">Pages</a>
                     <ul class="sub-menu">
                         <li><a href="{{ url('contact') }}">Contact Page</a></li>
